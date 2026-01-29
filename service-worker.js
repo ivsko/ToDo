784adf84d32f5a1689a8035d8ec90f7631b20fab
Ivan Kolev <88732318+ivsko@users.noreply.github.com>
2026-01-29 13:52:29 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'todo-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './firebase.js',
+  './manifest.json',
+  './icons/Cdemu_192x192.png',
+  './icons/Logo192.png.png',
+];
