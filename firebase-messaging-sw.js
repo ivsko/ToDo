ebbe5b452ad21c5f503b628a4de56fa85ca01865
Ivan Kolev <88732318+ivsko@users.noreply.github.com>
2026-01-29 17:04:23 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "…",
+  authDomain: "…",
+  projectId: "…",
+  messagingSenderId: "…",
+  appId: "…"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  });
+});
